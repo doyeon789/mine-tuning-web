70809708b5721135307630ac2fe966e61cdb0bd7
doyeon789 <ldyeon789@naver.com>
2026-06-22 16:12:49 +0900

    feat: media settings 추가

--- mine_tuning/urls.py
+++ mine_tuning/urls.py
@@ -1,4 +1,6 @@
-﻿from django.contrib import admin
+﻿from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
@@ -8,4 +10,7 @@ urlpatterns = [
     path('oauth/', include('allauth.urls')),
     path('', include('mine_chat.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
