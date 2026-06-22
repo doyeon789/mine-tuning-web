a80082918c8290dc145fed04264992b04c3dbb24
doyeon789 <ldyeon789@naver.com>
2026-06-22 16:13:01 +0900

    feat: post form 이미지 입력 추가

--- community/forms.py
+++ community/forms.py
@@ -6,7 +6,7 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ["title", "content"]
+        fields = ["title", "content", "image"]
         widgets = {
             "title": forms.TextInput(
                 attrs={
@@ -19,6 +19,12 @@ class PostForm(forms.ModelForm):
                     "class": "community-textarea",
                     "placeholder": "내용을 입력하세요",
                     "rows": 12,
+                }
+            ),
+            "image": forms.ClearableFileInput(
+                attrs={
+                    "class": "community-file-input",
+                    "accept": "image/jpeg,image/png,image/gif,image/webp",
                 }
             ),
         }
