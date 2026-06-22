51ac14a1fdad05e1748d433fc37c9971ab97d79b
doyeon789 <ldyeon789@naver.com>
2026-06-23 07:32:53 +0900

    fix: timezone 한국 시간 표시 수정

--- mine_tuning/settings.py
+++ mine_tuning/settings.py
@@ -133,7 +133,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -195,5 +195,6 @@ SOCIALACCOUNT_PROVIDERS = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
