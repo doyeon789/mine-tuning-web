003ddf82a8209d71a41769d8dca92f246a184b41
doyeon789 <ldyeon789@naver.com>
2026-06-22 15:53:12 +0900

    feat: community urls 작성

---
+++ community/urls.py
@@ -0,0 +1,13 @@
+﻿from django.urls import path
+
+from . import views
+
+app_name = "community"
+
+urlpatterns = [
+    path("", views.post_list, name="post_list"),
+    path("new/", views.post_create, name="post_create"),
+    path("<int:pk>/", views.post_detail, name="post_detail"),
+    path("<int:pk>/edit/", views.post_update, name="post_update"),
+    path("<int:pk>/delete/", views.post_delete, name="post_delete"),
+]
