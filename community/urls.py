6e68007ab55a0b6f3be6ca5635e37d92027e0989
. <.>
2026-06-22 17:40:38 +0900

    게시글 좋아요, 조회수 구현, 댓글까지 구현

--- community/urls.py
+++ community/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path("", views.post_list, name="post_list"),
     path("new/", views.post_create, name="post_create"),
     path("<int:pk>/", views.post_detail, name="post_detail"),
+    path("<int:pk>/like/", views.post_like, name="post_like"),
+    path("<int:pk>/comments/", views.comment_create, name="comment_create"),
     path("<int:pk>/edit/", views.post_update, name="post_update"),
     path("<int:pk>/delete/", views.post_delete, name="post_delete"),
 ]
--- community/views.py
+++ community/views.py
@@ -1,22 +1,36 @@
-﻿from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
+from django.http import JsonResponse
+from django.db.models import Count, F
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_POST
 
-from .forms import PostForm
+from .forms import CommentForm, PostForm
 from .models import Post
 
 
-@login_required
 def post_list(request):
-    posts = Post.objects.select_related("author")
+    posts = Post.objects.select_related("author").annotate(like_total=Count("liked_users"))
     return render(request, "community/post_list.html", {"posts": posts})
 
 
-@login_required
 def post_detail(request, pk):
-    post = get_object_or_404(Post.objects.select_related("author"), pk=pk)
-    return render(request, "community/post_detail.html", {"post": post})
+    post = get_object_or_404(
+        Post.objects.select_related("author").prefetch_related(
+            "liked_users",
+            "comments__author",
+        ),
+        pk=pk,
+    )
+    Post.objects.filter(pk=post.pk).update(view_count=F("view_count") + 1)
+    post.refresh_from_db(fields=["view_count"])
+    is_liked = request.user.is_authenticated and post.liked_users.filter(pk=request.user.pk).exists()
+    comment_form = CommentForm()
+    return render(
+        request,
+        "community/post_detail.html",
+        {"post": post, "is_liked": is_liked, "comment_form": comment_form},
+    )
 
 
 @login_required
@@ -61,6 +75,39 @@ def post_update(request, pk):
 
 @require_POST
 @login_required
+def post_like(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if post.liked_users.filter(pk=request.user.pk).exists():
+        post.liked_users.remove(request.user)
+        is_liked = False
+    else:
+        post.liked_users.add(request.user)
+        is_liked = True
+    if request.headers.get("x-requested-with") == "XMLHttpRequest":
+        return JsonResponse(
+            {
+                "is_liked": is_liked,
+                "like_count": post.liked_users.count(),
+            }
+        )
+    return redirect("community:post_detail", pk=post.pk)
+
+
+@require_POST
+@login_required
+def comment_create(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.author = request.user
+        comment.save()
+    return redirect("community:post_detail", pk=post.pk)
+
+
+@require_POST
+@login_required
 def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if post.author != request.user:
@@ -68,5 +115,3 @@ def post_delete(request, pk):
 
     post.delete()
     return redirect("community:post_list")
-
-
