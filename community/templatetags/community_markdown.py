b2f57ec7ecd37ace5843cac039467e03ae3c927b
doyeon789 <ldyeon789@naver.com>
2026-06-23 07:21:44 +0900

    feat: markdown template filter 작성

---
+++ community/templatetags/community_markdown.py
@@ -0,0 +1,51 @@
+﻿import bleach
+import markdown
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+ALLOWED_TAGS = [
+    "p",
+    "br",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "strong",
+    "em",
+    "del",
+    "blockquote",
+    "ul",
+    "ol",
+    "li",
+    "pre",
+    "code",
+    "hr",
+    "a",
+    "img",
+]
+ALLOWED_ATTRIBUTES = {
+    "a": ["href", "title"],
+    "img": ["src", "alt", "title"],
+}
+ALLOWED_PROTOCOLS = ["http", "https"]
+
+
+@register.filter(name="markdown")
+def render_markdown(value):
+    rendered = markdown.markdown(
+        value or "",
+        extensions=["fenced_code", "sane_lists", "nl2br"],
+        output_format="html",
+    )
+    cleaned = bleach.clean(
+        rendered,
+        tags=ALLOWED_TAGS,
+        attributes=ALLOWED_ATTRIBUTES,
+        protocols=ALLOWED_PROTOCOLS,
+        strip=True,
+    )
+    return mark_safe(cleaned)
